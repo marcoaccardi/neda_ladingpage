b75b5ef35e6cca6d153284247975ad04b022c9b3
sonotype <contact.marco.accardi@gmail.com>
2022-09-24 10:11:33 +0200

    add header and assets

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section>
-      <h2>A place where people meet science, art and technology</h2>
+    <section className='about'>
       <div className='about__container'>
         <div className='about__container-image'></div>
         <div className='about__container-text'></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import video from "../assets/video.mp4";
 const Header = () => {
   return (
-    <header>
-      <h1>Network for European Digital Arts</h1>
-    </header>
+    <>
+      <header className='header'>
+        <video autoPlay loop muted id='myVideo'>
+          <source src={video} type='video/mp4' />
+        </video>
+        <div>
+          <h1>Network for European Digital Arts</h1>
+          <h2>A place where people meet science, art and technology</h2>
+        </div>
+      </header>
+    </>
   );
 };
 
